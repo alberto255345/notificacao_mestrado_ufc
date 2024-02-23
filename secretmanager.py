f31f66e93e08190a20fae539d484f1e22e15c9b1
Alberto Vitoriano <alberto.vitoriano@ciadoleite.com.br>
2024-02-23 18:37:34 -0300

    go gogo

--- secretmanager.py
+++ secretmanager.py
@@ -5,6 +5,7 @@ def get_json_secret(project_id, secret_name):
     client = secretmanager.SecretsManagerClient()
     name = f"projects/{project_id}/secrets/{secret_name}/versions/latest"
     response = client.access_version(request={"name": name})
+    print(f"Response: {response}")
     
     decoded_value = response.payload.data.decode("UTF-8")
     return json.loads(decoded_value) if isinstance(decoded_value, str) else None
