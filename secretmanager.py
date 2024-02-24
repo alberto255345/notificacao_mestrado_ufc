77f4a5ef2fd0d5178f8d28f8a5d37f7b0edfea2d
Alberto Vitoriano <alberto.vitoriano@ciadoleite.com.br>
2024-02-24 11:41:38 -0300

    fadsfasdf

--- secretmanager.py
+++ secretmanager.py
@@ -5,7 +5,7 @@ def get_json_secret(project_id, secret_name):
     print(f"Project ID: {project_id}")
     print(f"Secret Name: {secret_name}")
     client = secretmanager.SecretManagerServiceClient()
-    name = f"projects/{project_id}/secrets/{secret_name}/versions/latest"
+    name = client.secret_path(project_id, secret_name)
     print(f"Name: {name}")
     response = client.get_secret(request={"name": name})
     # response = client.access_secret_version(request={"name": version.name})
