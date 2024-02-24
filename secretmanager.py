ad918664b0acb4ec3bdb21a25590d073805bdc84
Alberto Vitoriano <alberto.vitoriano@ciadoleite.com.br>
2024-02-24 12:03:11 -0300

    vfbgnhbjk

--- secretmanager.py
+++ secretmanager.py
@@ -4,13 +4,21 @@ import json
 def get_json_secret(project_id, secret_name):
     print(f"Project ID: {project_id}")
     print(f"Secret Name: {secret_name}")
+     # Create the Secret Manager client.
     client = secretmanager.SecretManagerServiceClient()
-    name = client.secret_path(project_id, secret_name)
-    print(f"Name: {name}")
-    response = client.get_secret(request={"name": name})
-    # response = client.access_secret_version(request={"name": version.name})
-    print(f"Response: {response}")
+
+    # Build the resource name of the secret version.
+    name = f"projects/{project_id}/secrets/{secret_name}/versions/latest"
+
+    # Get the secret version.
+    response = client.get_secret_version(request={"name": name})
+
+    # Print information about the secret version.
+    state = response.state.name
+    print(f"Got secret version {response.name} with state {state}")
+    # [END secretmanager_get_secret_version]
     
-    decoded_value = response.payload.data.decode("UTF-8")
+    decoded_value = response
+    # decoded_value = response.payload.data.decode("UTF-8")
     print(decoded_value)
     return json.loads(decoded_value) if isinstance(decoded_value, str) else None
