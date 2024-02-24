48a3b81705a04a63d1e653e15540cc8a1112454a
Alberto Vitoriano <alberto.vitoriano@ciadoleite.com.br>
2024-02-24 10:41:02 -0300

    teste

--- secretmanager.py
+++ secretmanager.py
@@ -4,10 +4,11 @@ import json
 def get_json_secret(project_id, secret_name):
     print(f"Project ID: {project_id}")
     print(f"Secret Name: {secret_name}")
-    client = secretmanager.SecretsManagerClient()
+    client = secretmanager.SecretManagerServiceClient()
     name = f"projects/{project_id}/secrets/{secret_name}/versions/latest"
     print(f"Name: {name}")
-    response = client.access_version(request={"name": name})
+    response = client.access_secret_version(request={"name": name})
+    # response = client.access_secret_version(request={"name": version.name})
     print(f"Response: {response}")
     
     decoded_value = response.payload.data.decode("UTF-8")
