96b623184563a4698c19ee898dd9f23d70914c72
Alberto Vitoriano <alberto.vitoriano@ciadoleite.com.br>
2024-03-14 10:22:36 -0300

    affasf

--- secretmanager.py
+++ secretmanager.py
@@ -4,6 +4,6 @@ import json
 def get_json_secret(project_id, secret_name):
     client = secretmanager.SecretManagerServiceClient()
     name = client.secret_version_path(project_id,secret_name,'latest')
-    response = client.access_secret_version(name) 
+    response = client.access_secret_version(name=name) 
     payload = response.payload.data.decode('UTF-8')
     return json.loads(payload) if isinstance(payload, str) else None
